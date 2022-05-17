bed5d0a30e5967cc1fdb07f5f1492415d46f442e
darya <lesoleill64@gmail.com>
2022-05-17 17:55:11 +0300

    add good response entity/ add category update

---
+++ src/main/java/com/example/ecommerce/common/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.example.ecommerce.common;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse {
+    private final boolean success;
+    private final String message;
+
+    public String getTimestamp() {
+        return LocalDateTime.now().toString();
+    }
+}
--- src/main/java/com/example/ecommerce/controller/CategoryController.java
+++ src/main/java/com/example/ecommerce/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.common.ApiResponse;
 import com.example.ecommerce.model.Category;
 import com.example.ecommerce.service.CategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,13 +17,23 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping("/create")
-    public String createCategory(@RequestBody Category category) {
+    public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
-        return "success";
+        return new ResponseEntity<>(new ApiResponse(true, "a new category created"), HttpStatus.CREATED);
     }
 
     @GetMapping("/list")
     public List<Category> listCategory() {
         return categoryService.listCategory();
+    }
+
+    @PostMapping("/update/{categoryId}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable int categoryId, @RequestBody Category category) {
+        System.out.println("category id:" + categoryId);
+        if (!categoryService.findById(categoryId)) {
+            return new ResponseEntity<>(new ApiResponse(true, "category doesn't exist"), HttpStatus.NOT_FOUND);
+        }
+        categoryService.editCategory(categoryId, category);
+        return new ResponseEntity<>(new ApiResponse(true, "category has been updated"), HttpStatus.OK);
     }
 }
